7f49afd721066ac661b4a3d081a18a958514dff9
Aniket Datta <datta.aniket7@gmail.com>
2023-08-05 02:04:11 +0530

    contact-formSubmit

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -29,37 +29,69 @@ export const Contact = ({ className }: ContactProps) => {
             [name]: value,
         }));
     };
+
+    // Form Submit function
+    const formSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        Object.entries(query).forEach(([key, value]) => {
+            formData.append(key, value);
+        });
+        const submitForm = await fetch(
+            'https://getform.io/f/8a3e3f67-0fb6-422f-a22d-b3172adb3333',
+            {
+                method: 'POST',
+                body: formData,
+            }
+        ).then(() => {
+            setQuery({ name: '', email: '', message: '' });
+            setSubmitted(true);
+        });
+    };
+
     return (
         <div className={classNames(styles.root, className)}>
-            <h2 className={styles['form-title']}>Get in touch 👋</h2>
-            <form className={styles.form}>
-                <label className={styles.label}>Name</label>
-                <input
-                    type="text"
-                    name="name"
-                    required
-                    value={query.name}
-                    onChange={handleParam()}
-                    className={styles.input}
-                />
-                <label className={styles.label}>Email</label>
-                <input
-                    type="text"
-                    name="email"
-                    required
-                    className={styles.input}
-                    value={query.email}
-                    onChange={handleParam()}
-                />
-                <label className={styles.label}>Message</label>
-                <input
-                    name="message"
-                    required
-                    className={styles.input}
-                    value={query.message}
-                    onChange={handleParam()}
-                />
-            </form>
+            {submitted && (
+                <div>
+                    <Lottie animationData={formAnimation} loop={false} />
+                </div>
+            )}
+            {!submitted && (
+                <>
+                    <h2 className={styles['form-title']}>Get in touch 👋</h2>
+                    <form onSubmit={formSubmit} className={styles.form}>
+                        <label className={styles.label}>Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            required
+                            value={query.name}
+                            onChange={handleParam()}
+                            className={styles.input}
+                        />
+                        <label className={styles.label}>Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            required
+                            className={classNames('form-control', styles.input)}
+                            value={query.email}
+                            onChange={handleParam()}
+                        />
+                        <label className={styles.label}>Message</label>
+                        <textarea
+                            name="message"
+                            required
+                            className={classNames('form-control', styles.input)}
+                            value={query.message}
+                            onChange={handleParam()}
+                        />
+                        <button type="submit" className={styles.submit}>
+                            Send
+                        </button>
+                    </form>
+                </>
+            )}
         </div>
     );
 };
