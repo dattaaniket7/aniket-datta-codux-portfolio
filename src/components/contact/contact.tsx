1e7cb7803924809970d67aaacb633b8692289d43
Aniket Datta <datta.aniket7@gmail.com>
2023-08-05 01:37:11 +0530

    contact-input

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,5 +1,8 @@
 import classNames from 'classnames';
 import styles from './contact.module.scss';
+import { useState } from 'react';
+import formAnimation from '../../assets/form-submitted.json';
+import Lottie from 'lottie-react';
 
 export interface ContactProps {
     className?: string;
@@ -10,5 +13,53 @@ export interface ContactProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Contact = ({ className }: ContactProps) => {
-    return <div className={classNames(styles.root, className)}>Contact</div>;
+    const [query, setQuery] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+    const [submitted, setSubmitted] = useState(false);
+
+    // Update inputs value
+    const handleParam = () => (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setQuery((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+    return (
+        <div className={classNames(styles.root, className)}>
+            <h2 className={styles['form-title']}>Get in touch 👋</h2>
+            <form className={styles.form}>
+                <label className={styles.label}>Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    required
+                    value={query.name}
+                    onChange={handleParam()}
+                    className={styles.input}
+                />
+                <label className={styles.label}>Email</label>
+                <input
+                    type="text"
+                    name="email"
+                    required
+                    className={styles.input}
+                    value={query.email}
+                    onChange={handleParam()}
+                />
+                <label className={styles.label}>Message</label>
+                <input
+                    name="message"
+                    required
+                    className={styles.input}
+                    value={query.message}
+                    onChange={handleParam()}
+                />
+            </form>
+        </div>
+    );
 };
